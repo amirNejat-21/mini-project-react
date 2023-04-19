e72675b55ef73ec75048739ecf130df997bb25be
Amir Nejat <amirnejat310@gmail.com>
2023-04-19 07:46:46 +0330

    made the Search part with styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Banner from "./components/Banner";
 import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+
 function App() {
   return (
     <div >
      <Navbar/>
      <Banner/>
      <Cards/>
+     <Search/>
     </div>
   );
 }
