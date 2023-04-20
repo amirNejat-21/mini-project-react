6bf87eeb5c81792f0ec7abd4b1596aac31c1e77c
Amir Nejat <amirnejat310@gmail.com>
2023-04-20 12:59:00 +0330

    added the prodect section

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+
+import Card from './Card';
+
+// styles
+import styles from "./product.module.css"
+
+// API
+import { getProducts } from '../API/api';
 
 const Product = () => {
+     const [product,setProduct] = useState([])
+   useEffect(() => {
+        const fetchAPI = async () =>{
+            const data= await getProducts();
+            setProduct(data)
+        }
+
+        fetchAPI()
+   },[])
+
+
+    
     return (
-        <div>
-            Products
+        <div className={styles.container }>
+           {product.map(product => <Card key={product.id} image={product.image} name={product.title} cost={product.price}/>)}
         </div>
     );
 };
